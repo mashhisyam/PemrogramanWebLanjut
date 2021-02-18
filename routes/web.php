2a08fa0dfae7b1fc5cff477d01ed21fefd3a97f0
maulana156357 <maulanamalik23145@gmail.com>
2021-02-18 10:07:42 +0700

    Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo "1941720055 <br>"; 
     echo "Maulana Malik Ibrahim <br>"; 
-    echo "TI-2A";
+    echo "TI - 2A";
 });
 //3
 Route::get('/articles/{id}', function ($id) {
