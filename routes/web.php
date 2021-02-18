fe2405d84c2b754da56f924d7e1153bee87e6b70
Maulana23145 <62891893+Maulana23145@users.noreply.github.com>
2021-02-18 12:44:22 +0700

    Praktikum3

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\HomeController;
 use App\http\Controllers\Article;
-use App\http\Controllers\About;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,8 +31,52 @@ nomer 3
 Route::get('/articles/{id}', function ($id) {
     echo "Halaman Artikel dengan ID".$id;
 });
-*/
-//Praktikum 2
+
+Praktikum 2
 Route::get('/', [HomeController::class,'index']);
 Route::get('/article/{id}', [Article::class,'article']);
 Route::get('/about', [About::class,'about']);
+*/
+
+//Praktikum 3
+//1
+Route::get('/', [HomeController::class,'index']);
+//2
+Route::prefix('product')->group(function(){
+    Route::get('pakaian', function () {
+        echo "Pakaian"; 
+    });
+    Route::get('makanan', function () {
+        echo "Daging"; 
+    });
+    Route::get('minuman', function () {
+        echo "Coca-cola"; 
+    });
+});
+//3
+Route::get('/article/{id}', [Article::class,'article']);
+
+//4
+Route::prefix('program')->group(function(){
+    Route::get('melukis', function () {
+        echo "Program Melukis"; 
+    });
+    Route::get('fitnes', function () {
+        echo "Program Fitnes"; 
+    });
+    Route::get('design', function () {
+        echo "Program Design"; 
+    });
+});
+//5
+Route::get('/about', function () {
+    echo "1941720055 <br>"; 
+    echo "Maulana Malik Ibrahim <br>"; 
+    echo "TI-2A";
+});
+//6
+Route::get('/contact', function () {
+    echo "For more information, please Contact 021-245-34456"; 
+});
+
+
