ae888a3045781c23de3fead1852182f14b406470
Maulana23145 <62891893+Maulana23145@users.noreply.github.com>
2021-02-18 10:21:20 +0700

    Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Praktikum 1
+//nomer1
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+//nomer 2
+Route::get('/about', function () {
+    echo "1941720055 <br>"; 
+    echo "Maulana Malik Ibrahim <br>"; 
+    echo "TI-2A";
+});
+//nomer 3
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID".$id;
 });
